219e166e0d708baa92a19824fb6c0f5f33f27cb7
TenzinGayche <TenzinGayche@users.noreply.github.com>
2023-09-28 05:24:58 +0000

    ✅ Ready to clone and code.

---
+++ num2tib/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for num2tib."""
+
+from num2tib.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from num2tib.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "num2tib"
